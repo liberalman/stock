078186ff97810e69f25aec6743af198c0fc3be8e
liberalman <zscchina@163.com>
2019-03-09 16:17:16 +0800

    do.sh

--- do.sh
+++ do.sh
@@ -10,7 +10,7 @@ python3 analysis.py
 
 content=`cat ../tmp/沪深龙虎榜统计_*.md`
 curl -d "text=龙虎榜统计&desp=${content}" \
-    https://sc.ftqq.com/SCU46037Tfbc1e16cfe0dcff0fca6a416ff5644fa5c8347a9281db.send
+    https://sc.ftqq.com/${token}.send
 
 popd
 
