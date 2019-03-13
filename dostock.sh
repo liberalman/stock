fb24381e823633cf15a9488107a1886752adcb13
shouchao.zheng <shouchao.zheng@nio.com>
2019-03-13 11:48:46 +0800

    dostock

---
+++ dostock.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+source /etc/profile
+
+# add crontab
+exist=`crontab -l|grep stock`
+if [ -z "${exist}" ];
+  crontab -l > /tmp/crontab.bak
+  echo '0 23 * 1,2,3,4,5 * /data/stock/dostock.sh > /var/log/dostock.log 2>&1 &' >> /tmp/crontab.bak
+  crontab /tmp/crontab.bak
+  rm -f /tmp/crontab.bak
+fi
+
+# dostock
+pushd /data/stock/tiger_list
+
+python3 fetch.py
+python3 analysis.py
+
+token=""
+day=`date "+\%Y-\%m-\%d"`
+content=`cat ../tmp/沪深龙虎榜统计_${day}.md`
+curl -d "text=龙虎榜统计&desp=${content}" \
+    https://sc.ftqq.com/${token}.send
+
+popd
+
