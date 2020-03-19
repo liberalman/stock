8aae44bb6cd3480e46df1cb2cfd8da58ad7450a8
liberalman <zscchina@163.com>
2020-03-19 17:59:13 +0800

    change

--- dostock.sh
+++ dostock.sh
@@ -6,7 +6,7 @@ source /etc/profile
 exist=`crontab -l|grep stock`
 if [ -z "${exist}" ];then
   crontab -l > /tmp/crontab.bak
-  echo '0 23 * 1,2,3,4,5 * /data/stock/dostock.sh > /var/log/dostock.log 2>&1 &' >> /tmp/crontab.bak
+  echo '0 21 * 1-5 * /data/stock/dostock.sh > /var/log/dostock.log 2>&1 &' >> /tmp/crontab.bak
   crontab /tmp/crontab.bak
   rm -f /tmp/crontab.bak
 fi
@@ -19,7 +19,7 @@ python3 analysis.py
 
 token=""
 day=`date "+%Y-%m-%d"`
-content=`cat ../tmp/沪深龙虎榜统计_${day}.md`
+content=`cat ../tmp/1沪深龙虎榜统计_${day}.md`
 curl -d "text=龙虎榜统计&desp=${content}" \
     https://sc.ftqq.com/${token}.send
 
